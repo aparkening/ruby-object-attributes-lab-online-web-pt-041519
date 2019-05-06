de0069ae3d9595ac492559060771ac4a6e86a05e
aparkening <aaron.parkening@gmail.com>
2019-05-06 15:21:44 -0400

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,16 @@
+# Define class with name and breed instance methods
+class Dog
+  def name
+    @name
+  end
+  def name=(new_name)
+    @name = new_name
+  end
+
+  def breed
+    @breed
+  end
+  def breed=(new_breed)
+    @breed = new_breed
+  end
+end
---
+++ lib/person.rb
@@ -0,0 +1,16 @@
+# Define class with name and job instance methods
+class Person
+  def name
+    @name
+  end
+  def name=(new_name)
+    @name = new_name
+  end
+
+  def job
+    @job
+  end
+  def job=(new_job)
+    @job = new_job
+  end
+end
